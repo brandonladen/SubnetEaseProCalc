dfd95f6594ffdfac15a66eb7e70e58ae2d3d530e
brandonladen <brandonladen486@gmail.com>
2024-06-09 16:37:56 +0300

    removed unnecessay codes

--- authentication/forms.py
+++ authentication/forms.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class userprofileform(forms.ModelForm):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['first_name', 'last_name', 'email', 'username']
