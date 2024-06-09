54e2565339f93c49faf50b3fdc1503f70b228403
brandonladen <brandonladen486@gmail.com>
2024-06-09 17:18:57 +0300

    Added signup functionality

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_user, logout_user, update_profile
+from .views import login_user, logout_user, update_profile, signup_user
 
 urlpatterns = [
     path('login/', login_user, name='login'),
+    path('signup/', signup_user, name='signup'),
     path('logout/', logout_user, name='logout'),
     path('update_profile/', update_profile, name='update_profile'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,9 +3,37 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import userprofileform
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+def signup_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+         # Check if passwords match
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match")
+            return redirect('signup_user')
+        
+         # Check if username is already taken
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username is already taken")
+            return redirect('signup_user')
+        
+         # Create the user object
+        user = User.objects.create_user(username=username, 
+                                        email=email,
+                                        password=password)
+        user.save()        
+        messages.success(request, "Successfully signed up. You can now login.")
+        return redirect('login')
+            
+    return render(request, "signup.html")
+
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
